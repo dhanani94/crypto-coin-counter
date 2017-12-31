8a6b52e18dd7fd0890ef75d3c792c8f3cbb464f1
Taufiq Dhanani <Taufiq@taufiqs-mini.attlocal.net>
2017-12-31 17:00:57 -0500

    creates new table for holdings

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA crypto_counter;
 CREATE TABLE IF NOT EXISTS crypto_counter.cc_current_price (
     id          uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
-    currency    varchar(10) NOT NULL,
+    currency    varchar(25) NOT NULL,
     price       numeric NOT NULL,
    	created_at 	timestamp DEFAULT NOW()
 );
@@ -11,4 +11,10 @@ CREATE TABLE IF NOT EXISTS crypto_counter.cc_wallet_balance (
     id          uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     balance     money NOT NULL,
    	created_at 	timestamp DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS crypto_counter.holdings (
+    symbol    varchar(10) PRIMARY KEY,
+    name 	  varchar(25),
+    holding  numeric NOT NULL
 );
